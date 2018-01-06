73325e5ab1d993f721f1b9545b316f53d743d760
p2bilt <peteralan@gmail.com>
2018-01-05 22:21:56 -0500

    clean up code commenting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,7 +20,7 @@ var quiz = [{
             "U-No-Poo"
         ],
         "correct": "Metamorph-Medals",
-        "explanation": "First mentioned by Arthur Weasley in the Half-Blood Prince, these magical scam objects, far from helping the wearer metamorph, often backfire dramatically.",
+        "explanation": "First mentioned by Arthur Weasley in the HALF-BLOOD PRINCE, these magical scam objects, far from helping the wearer metamorph, often backfire dramatically.",
     },
     {
         "question": "What is the only antidote to Basilisk venom?",
@@ -44,7 +44,7 @@ var quiz = [{
             "An Elk"
         ],
         "correct": "A Lion",
-        "explanation": "A transparent metaphor for the bravery of this house's members, this symbol was personally chosen by Godric Gryffindor himself.",
+        "explanation": "A lion is a transparent metaphor for the bravery of this house's members, this symbol was personally chosen by Godric Gryffindor himself.",
     },
     {
         "question": "In his youth, prior to the troubles with You-Know-Who, Dumbledore is famous for single-handedly defeating and imprisoning which Dark wizard?",
@@ -94,14 +94,11 @@ var quiz = [{
         "correct": "Cedric Diggory",
         "explanation": "When Cedric, along with Harry, falls into Voldemort's trap at the end of THE GOBLET OF FIRE, He-Who-Shall-Not-Be-Named orders Peter Pettigrew to \"Kill the spare\" -- Peter then murders Cedric with the Killing Curse.",
     },
-
 ];
 
-// Set our variables
+// variables
 var currentquestion,
-    score,
-    submt;
-
+    score;
 
 function htmlEncode(value) {
     return $(document.createElement('div')).text(value).html();
@@ -113,11 +110,9 @@ function addChoices(choices) {
     for (var i = 0; i < choices.length; i++) {
         $(document.createElement('li')).addClass('choice choice-box').attr('data-index', i).text(choices[i]).appendTo('#choice-block');
     }
-
 }
 
 function nextQuestion() {
-    submt = true;
     $('#explanation').empty();
     $('#question').text(quiz[currentquestion]['question']);
     $("#test_status").html('Question ' + Number(currentquestion + 1) + ' of ' + quiz.length);
@@ -138,7 +133,6 @@ function processQuestion(choice) {
     } else {
         nextQuestion();
     }
-
 }
 
 
@@ -166,18 +160,7 @@ function setupButtons() {
             cdreset();
             countdown();
         }
-        // $("#submitbutton").show();
-
-        if (submt) {
-            submt = false;
-
-            function nextquest() {
-                $('.choice').off('click');
-                $(this).off('click');
-                processQuestion(choice);
-            }
-        }
-    })
+    });
 }
 
 
@@ -195,21 +178,15 @@ function endQuiz() {
 
 
 function init() {
-
+    // initialize variables
     currentquestion = 0;
     score = 0;
-    submt = true;
     //add first question tracker and questions
-
     //add pager (question tracker)
     $("#test_status").html('Question ' + Number(currentquestion + 1) + ' of ' + quiz.length);
-    // $(document.createElement('p')).addClass('pager').attr('id', 'pager').text('Question 1 of ' + quiz.length).appendTo('#frame');
     //add first question
     $(document.createElement('h2')).addClass('question').attr('id', 'question').text(quiz[0]['question']).appendTo('#frame');
-    //add image if present
-    // if (quiz[0].hasOwnProperty('image') && quiz[0]['image'] != "") {
-    //     $(document.createElement('img')).addClass('question-image').attr('id', 'question-image').attr('src', quiz[0]['image']).attr('alt', htmlEncode(quiz[0]['question'])).appendTo('#frame');
-    // }
+
     $(document.createElement('p')).addClass('explanation').attr('id', 'explanation').appendTo('#frame');
 
     //questions holder
@@ -218,16 +195,8 @@ function init() {
     //add choices
     addChoices(quiz[0]['choices']);
 
-    //add submit button
-    // $(document.createElement('div')).addClass('choice-box').attr('id', 'submitbutton').text('Next Question').css({
-    //     'font-weight': 700,
-    //     'color': '#222',
-    //     'padding': '30px 0',
-    // }).appendTo('#frame');
-
-    // $("#submitbutton").hide();
-
     setupButtons();
+
     // timer
     cdreset();
     countdown("modal");
@@ -238,12 +207,13 @@ function beginbutton(startend) {
     $(document.createElement('ul')).attr('id', 'choice-block').appendTo('#frame');
     $(document.createElement('li')).addClass('begin choice choice-box').text(startend + " GAME").appendTo('#choice-block');
 }
+
 // create a begin button at start of game
 beginbutton("BEGIN");
 
 // wait for click of begin button to initialize game
 
-$(document).on('click','.begin',function(){
+$(document).on('click', '.begin', function() {
     console.log("begin button clicked");
     $('#frame').empty();
     init();
